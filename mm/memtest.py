31e120b48be5cbdd57a829b59699c76a2b76b4b4
0xcc-Since2016 <0xccsince2016@gmail.com>
2018-09-22 23:24:42 -0700

    update memset

--- mm/memtest.py
+++ mm/memtest.py
@@ -23,15 +23,17 @@ class Processer:
         self.pid = pid
         self.map_path = "/proc/" + self.pid + "/maps"
         self.mem_path = "/proc/" + self.pid + "/mem"
+        self.fp_map = open(self.map_path, "rb")
+        self.fp_mem = open(self.mem_path, "rb")
+        self.maps = self.fp_map.read()
 
     def pick_mem(self, offset, length):
 
-        fp_ = open(self.mem_path , 'rb')
-        fp_.seek(offset)
-        content = fp_.read(length)
+        self.fp_mem.seek(offset)
+        content = self.fp_mem.read(length)
         return content
 
-def mem(pid):
+def mm(pid):
 
     pid = str(pid)
     pid = pid.strip("[").strip("]")
